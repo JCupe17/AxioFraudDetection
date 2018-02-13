cbfc34d97e3c3608e096f807197aa30acfdf4676
José SANCHEZ <jose.sanchez@axionable.com>
2018-02-13 16:28:57 +0100

    Fichier des fonctions Axionable à utiliser dans ce projet

---
+++ source/fonctions.py
@@ -0,0 +1,11 @@
+import re
+import unidecode
+
+def format_column_name(name):
+    """Format a string that will be used as a column name
+    """
+    name = re.sub(r"(\.|°)", "", name)  # Changer les spaces par ""
+    name = unidecode(name)  # Supprimer les caractères spéciaux
+    name = name.lower()  # Passer les noms en minuscule
+    name = re.sub(r"(\s)", "_", name)  # Changer les spaces par ""
+    return name
